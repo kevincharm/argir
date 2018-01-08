59fbf3850e867f8056106a35172ef5641aa18523
Kevin Charm <kevin@kevincharm.com>
2018-01-08 22:01:10 +0800

    use stdio in kernel_main

--- src/kernel.c
+++ src/kernel.c
@@ -1,16 +1,17 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <stdio.h>
 #include "terminal.h"
 
 void kernel_main()
 {
     terminal_init();
-    terminal_write(
+    printf(
         "Argir i386\n"
         "Build "__ARGIR_BUILD_COMMIT__"\n\n"
     );
     for (uint8_t i=0; i<80; i++) {
-        terminal_write_char('0'+i);
+        putchar('0'+i);
     }
 }
