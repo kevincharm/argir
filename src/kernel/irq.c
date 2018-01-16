7cbeb61ed7b1a3f624cc1159949b6f783fe68011
Kevin Charm <kevin@kevincharm.com>
2018-01-16 15:58:35 +0800

    add missing void ptr cast

--- src/kernel/irq.c
+++ src/kernel/irq.c
@@ -7,7 +7,7 @@ void irq_init()
 {
     // TODO: replace initialisation with memset
     for (size_t i=0; i<256; i++) {
-        uint8_t *u8idt = idt+i;
+        uint8_t *u8idt = (void *)(idt+i);
         for (size_t j=0; j<3; j++)
             u8idt[j] = 0;
     }
