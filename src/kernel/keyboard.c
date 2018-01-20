033d50564022363f3bc9bfd6193de963f63c7f6e
Kevin Charm <kevin@kevincharm.com>
2018-01-20 22:59:56 +0800

    PS/2 fixes

--- src/kernel/keyboard.c
+++ src/kernel/keyboard.c
@@ -56,10 +56,11 @@ static void kb_scan_and_push()
 void keyboard_irq_handler()
 {
     irq_test = true;
-    kb_scan_and_push();
+    printf("Keyboard IRQ handler called!\n");
+    // kb_scan_and_push();
 }
 
-static void ps2_wait_cmd(uint16_t port, uint8_t data)
+static void ps2_wait_write(uint16_t port, uint8_t data)
 {
     uint8_t ret;
     do {
@@ -77,7 +78,7 @@ static uint8_t ps2_wait_read(uint16_t port)
     return inb(port);
 }
 
-static void ps2_flush_input()
+static void ps2_flush_output()
 {
     uint8_t ret;
     do {
@@ -96,32 +97,37 @@ void keyboard_init()
     // 8042 initialisation
     uint8_t ret;
 
-    // Disable first PS/2 port
+    // Disable PS/2 ports
     outb(PS2_PORT_STATCMD, 0xad);
+    outb(PS2_PORT_STATCMD, 0xa7);
 
     // Flush output buffer
-    ps2_flush_input();
+    ps2_flush_output();
 
     // Read config
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0x20);
+    ps2_wait_write(PS2_PORT_STATCMD, 0x20);
     ret = ps2_wait_read(PS2_PORT_DATA);
+    if (!((ret >> 2u) & 0x01)) {
+        printf("8042: Config = %u\n", ret);
+        printf("8042: System did not pass POST!\n");
+    }
     uint8_t config = ret;
     config &= ~(1u << 0u); /* PS/2 IRQ1 */
     config &= ~(1u << 1u); /* PS/2 IRQ12 */
     config &= ~(1u << 6u); /* Translation */
     // Write config
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0x60);
-    ps2_wait_cmd(PS2_PORT_DATA, config);
+    ps2_wait_write(PS2_PORT_STATCMD, 0x60);
+    ps2_wait_write(PS2_PORT_DATA, config);
 
     // Self-test
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0xaa);
+    ps2_wait_write(PS2_PORT_STATCMD, 0xaa);
     ret = ps2_wait_read(PS2_PORT_DATA);
     if (ret != 0x55) {
         printf("8042: self-test failed!\n");
     }
 
     // Test first PS/2 port
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0xab);
+    ps2_wait_write(PS2_PORT_STATCMD, 0xab);
     ret = ps2_wait_read(PS2_PORT_DATA);
     switch (ret) {
     case 0x00:
@@ -143,44 +149,47 @@ void keyboard_init()
     }
 
     // Enable first PS/2 port
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0xae);
+    ps2_wait_write(PS2_PORT_STATCMD, 0xae);
 
     // Read config
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0x20);
+    ps2_wait_write(PS2_PORT_STATCMD, 0x20);
     ret = ps2_wait_read(PS2_PORT_DATA);
     config = ret;
-    config |= (1u << 0u); /* bit 0 set -> enable PS/2 #1 int */
-    config |= (1u << 4u);
+    config |= (1u << 0u); /* IRQ1 */
+    config &= ~(1u << 4u); /* Enable PS/2 port clock */
+    config &= ~(1u << 6u); /* Translation */
+    config &= ~(1u << 7u); /* Must be zero */
     // Write config
-    ps2_wait_cmd(PS2_PORT_STATCMD, 0x60);
-    ps2_wait_cmd(PS2_PORT_DATA, config);
+    ps2_wait_write(PS2_PORT_STATCMD, 0x60);
+    ps2_wait_write(PS2_PORT_DATA, config);
+    printf("8042: Wrote PS/2 config = %u\n", config);
 
     // Set autorepeat -> 500ms
-    ps2_wait_cmd(PS2_PORT_DATA, 0xf3);
-    ps2_wait_cmd(PS2_PORT_DATA, 0x20);
+    ps2_wait_write(PS2_PORT_DATA, 0xf3);
+    ps2_wait_write(PS2_PORT_DATA, 0x20);
 
     // Set scancode -> set 2
-    ps2_wait_cmd(PS2_PORT_DATA, 0xf0);
-    ps2_wait_cmd(PS2_PORT_DATA, 0x02);
+    ps2_wait_write(PS2_PORT_DATA, 0xf0);
+    ps2_wait_write(PS2_PORT_DATA, 0x02);
 
     // Flags
-    ps2_wait_cmd(PS2_PORT_DATA, 0xfa);
+    ps2_wait_write(PS2_PORT_DATA, 0xfa);
 
     // Enable scanning
-    ps2_wait_cmd(PS2_PORT_DATA, 0xf4);
+    ps2_wait_write(PS2_PORT_DATA, 0xf4);
     ret = ps2_wait_read(PS2_PORT_DATA);
     if (ret != 0xfa) {
         printf("PS/2 enable scanning failed!\n");
     }
 
     // Reset PS/2 device
-    ps2_wait_cmd(PS2_PORT_DATA, 0xff);
+    ps2_wait_write(PS2_PORT_DATA, 0xff);
     ret = ps2_wait_read(PS2_PORT_DATA);
     if (ret != 0xfa) {
         printf("PS/2 reset failed!\n");
     }
 
-    ps2_flush_input();
+    ps2_flush_output();
     printf("Keyboard initialised!\n\n");
 }
 
@@ -192,7 +201,7 @@ void keyboard_main()
         printf("a");
     }
 
-    while (kbuf->len) {
-        putchar(lifo_pop(kbuf));
-    }
+    // while (kbuf->len) {
+        // putchar(lifo_pop(kbuf));
+    // }
 }
