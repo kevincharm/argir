c0342619482c6621488918661c3f6d7d4d61872a
Kevin Tjiam <kevin@kevincharm.com>
2018-03-05 15:31:07 +0800

    implement outw, outl, inw, inl

--- src/include/kernel/io.h
+++ src/include/kernel/io.h
@@ -12,11 +12,51 @@ static inline void outb(uint16_t port, uint8_t byte)
     );
 }
 
+static inline void outw(uint16_t port, uint16_t word)
+{
+    __asm__ volatile (
+        "outw %0, %1"
+        :
+        : "a" (word), "Nd" (port)
+    );
+}
+
+static inline void outl(uint16_t port, uint32_t dword)
+{
+    __asm__ volatile (
+        "outl %0, %1"
+        :
+        : "a" (dword), "Nd" (port)
+    );
+}
+
 static inline uint8_t inb(uint16_t port)
 {
     uint8_t ret;
     __asm__ volatile (
         "inb %1, %0"
+        : "=r" (ret)
+        : "Nd" (port)
+    );
+    return ret;
+}
+
+static inline uint16_t inw(uint16_t port)
+{
+    uint16_t ret;
+    __asm__ volatile (
+        "inw %1, %0"
+        : "=r" (ret)
+        : "Nd" (port)
+    );
+    return ret;
+}
+
+static inline uint32_t inl(uint16_t port)
+{
+    uint32_t ret;
+    __asm__ volatile (
+        "inl %1, %0"
         : "=r" (ret)
         : "Nd" (port)
     );
